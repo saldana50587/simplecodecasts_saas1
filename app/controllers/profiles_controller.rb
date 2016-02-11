57d141cd2c4fd72a6ad884001ea9ed806e1e02ad
Hector R. Saldana <hector.saldana@me.com>
2016-02-11 01:13:27 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
  
+  before_action :authenticate_user!
+  before_action :only_current_user
+ 
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -14,13 +17,42 @@ class ProfilesController < ApplicationController
       flash[:success] = "Profile Updated!"
       redirect_to user_path( params[:user_id] ) #If save works lets redirect user to another page (show_page)
     else
-      render action: :new #If it fails, load the "new" method from the above. 
+      render action: :new #If it fails, load the "new" method from above. 
     end
   end
+  
+    def edit
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+    end
+    
+    def update
+  @user = User.find( params[:user_id] )
+  @profile = @user.profile
+  
+  if @profile.update_attributes(profile_params)
+    flash[:success] = "Profile Updated!"
+    redirect_to user_path( params[:user_id] )
+  else
+    render action: :edit
+  end
+end
   
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
+    
+  # Warning: Anything below this line will become private as well. 
+    
+    
 end
   
+
+
+
