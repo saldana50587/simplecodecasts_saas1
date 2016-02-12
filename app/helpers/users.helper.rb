18949e82491d6e20360c0ba67d507b404652a807
Hector R. Saldana <hector.saldana@me.com>
2016-02-12 02:58:04 +0000

    Improvement to user profiles pages

---
+++ app/helpers/users.helper.rb
@@ -0,0 +1,10 @@
+module UsersHelper
+    
+    def job_title_icon
+       if @user.profile.job_title == "Developer"
+         "<i class='fa fa-code'></i>".html_safe
+       end
+    end
+end
+    
+
